51e8c299e122372ab8746c30d59e7eb986249bb6
Ankit <jswlakt@gmail.com>
2025-07-28 04:20:56 +0530

    Nested Dict

---
+++ Dictionary/nested_dictionary.py
@@ -0,0 +1,20 @@
+
+student = {
+    "name": "Ankit",
+    "marks": {
+        "robotics": 8,
+        "iot": 6
+
+    }
+}
+
+print(student)
+print("----------------------------------")
+
+print(student["name"])
+print("----------------------------------")
+
+print(student["marks"])
+print("----------------------------------")
+
+print(student["marks"]["robotics"])
