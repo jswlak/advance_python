9203ee13bfc9ef38a93f08207ada47bcbcb0364e
Ankit <jswlakt@gmail.com>
2025-07-28 03:59:50 +0530

    Learn Dictionary

---
+++ Dictionary/dictionary_basic.py
@@ -0,0 +1,20 @@
+#creating dicitonary - Dictionary is a key value pair
+
+my_dict = {
+    "name" : "Ankit",
+    "age" : 24,
+    "city": "Patna"
+
+}
+
+print(my_dict)
+
+#Acessing value
+
+print(my_dict["city"])
+
+#empty dictionary
+
+emp = {}
+print(emp)
+
