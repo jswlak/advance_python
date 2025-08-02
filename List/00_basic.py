8b730e1b2989a78392f30f03f49d239420eeadaa
Ankit <jswlakt@gmail.com>
2025-08-02 23:35:37 +0530

    Learn about list in python

---
+++ List/00_basic.py
@@ -0,0 +1,27 @@
+# A list is a collection that is ordered, changeable, and allows duplicate values.
+
+fruit = ["Apple", "Banana", "Papaya", "Mango", "Litchi", "Blueberry", "Grava", "Pineapple"]
+
+print(fruit)
+
+# Creating list
+
+number = [1, 2,3]
+name = ["Ankit", "Naina", "Sona"]
+mixed = [1, "Apple", "Ak", True, "hello", 2.66]
+empty = []
+
+
+# Accessing Elements
+
+print(mixed[2])                 #print 2nd index value - of list mixed
+print(mixed[2][1])              #print 1st index letter- of word at 2nd index value . i.e word is Ak, and "k" is its 1st index letter
+
+print(name[-1])                 # last item
+
+#Modifying Elements
+
+name[2] = "Mona"                #replace existig with new value
+print(name)
+
+
