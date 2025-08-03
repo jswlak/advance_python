afd222d7776b625f940b5628e7dd9631a197ebcd
Ankit <jswlakt@gmail.com>
2025-08-03 08:06:20 +0530

    solve to sum item in list

---
+++ List/assignment/00_item_sum.py
@@ -0,0 +1,15 @@
+# Sum of Elements
+# Write a program to compute the sum of all elements in a list.
+
+    # # Input: [1, 2, 3, 4]
+    # Output: 10
+
+
+numbers = [1, 2, 3, 4]
+
+total = 0 #emp list to store the sum
+
+for i in numbers:
+    total += i
+
+print(total)
