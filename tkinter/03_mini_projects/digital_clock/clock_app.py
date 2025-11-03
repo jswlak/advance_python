6471ca559d95bd0fa42bc9c927517a836a54d8a6
Ankit <jswlakt@gmail.com>
2025-11-04 00:03:49 +0530

    change the dimension of windows to 400x200

--- tkinter/03_mini_projects/digital_clock/clock_app.py
+++ tkinter/03_mini_projects/digital_clock/clock_app.py
@@ -4,7 +4,7 @@ import time
 
 root = Tk()
 root.title("Digital Clock")
-root.geometry("300x150")
+root.geometry("400x200")
 
 #Label to Show Time
 time_label = Label(root, text="00:00:00", font=("Helvetica", 48), fg="cyan", bg="black")
