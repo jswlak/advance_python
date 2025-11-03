3f18e369a7a712afc97a86c81188a8666e7cfa7c
Ankit <jswlakt@gmail.com>
2025-11-03 23:40:36 +0530

    add label to show time

--- tkinter/03_mini_projects/digital_clock/clock_app.py
+++ tkinter/03_mini_projects/digital_clock/clock_app.py
@@ -4,4 +4,10 @@ root = Tk()
 root.title("Digital Clock")
 root.geometry("300x150")
 
+#Label to Show Time
+time_label = Label(root, text="00:00:00", font=("Helvetica", 48), fg="cyan", bg="black")
+time_label.pack(expand=True, fill="both")
+
+
+
 root.mainloop()
