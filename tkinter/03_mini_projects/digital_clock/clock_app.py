b875c22abcc66c43a42d2a64d69bb70b5252b50d
Ankit <jswlakt@gmail.com>
2025-11-03 23:49:57 +0530

    add update_time function to update the time automatically

--- tkinter/03_mini_projects/digital_clock/clock_app.py
+++ tkinter/03_mini_projects/digital_clock/clock_app.py
@@ -12,7 +12,14 @@ time_label.pack(expand=True, fill="both")
 
 
 #Fetch Current Time
-current_time = time.strftime("%H:%M:%S")
-print(current_time)
+# current_time = time.strftime("%H:%M:%S")
+# print(current_time)
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    time_label.config(text=current_time)
+    time_label.after(1000, update_time)  # recall function every second
+
+update_time()
 
 root.mainloop()
