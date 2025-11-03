93af11c4d16bd5694ecd45d7d7b355fc085bf2d2
Ankit <jswlakt@gmail.com>
2025-11-03 23:43:20 +0530

    fetch current time

--- tkinter/03_mini_projects/digital_clock/clock_app.py
+++ tkinter/03_mini_projects/digital_clock/clock_app.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+import time
+
 root = Tk()
 root.title("Digital Clock")
 root.geometry("300x150")
@@ -9,5 +11,8 @@ time_label = Label(root, text="00:00:00", font=("Helvetica", 48), fg="cyan", bg=
 time_label.pack(expand=True, fill="both")
 
 
+#Fetch Current Time
+current_time = time.strftime("%H:%M:%S")
+print(current_time)
 
 root.mainloop()
