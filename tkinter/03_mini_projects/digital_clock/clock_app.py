2aa04fd9bdc472100d3193670cd1d0e88f30af29
Ankit <jswlakt@gmail.com>
2025-11-04 00:01:49 +0530

    upadate style and add date

--- tkinter/03_mini_projects/digital_clock/clock_app.py
+++ tkinter/03_mini_projects/digital_clock/clock_app.py
@@ -10,6 +10,13 @@ root.geometry("300x150")
 time_label = Label(root, text="00:00:00", font=("Helvetica", 48), fg="cyan", bg="black")
 time_label.pack(expand=True, fill="both")
 
+#add style
+root.configure(bg="black")
+time_label.config(fg="lime", font=("Courier", 48, "bold"))
+
+#date
+date_label = Label(root, text="", font=("Helvetica", 18), fg="white", bg="black")
+date_label.pack(side="bottom", fill="x")
 
 #Fetch Current Time
 # current_time = time.strftime("%H:%M:%S")
@@ -17,7 +24,9 @@ time_label.pack(expand=True, fill="both")
 
 def update_time():
     current_time = time.strftime("%H:%M:%S")
+    current_date = time.strftime("%A, %d %B %Y") #current date
     time_label.config(text=current_time)
+    date_label.config(text=current_date)
     time_label.after(1000, update_time)  # recall function every second
 
 update_time()
